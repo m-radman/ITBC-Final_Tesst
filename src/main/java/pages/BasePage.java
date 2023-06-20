5653fd6006ee07cc97c669d05dcc841e2160fc1e
m-radman <milrad@live.com>
2023-06-20 16:20:23 +0200

    Add BasePage

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,22 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    public BasePage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public WebDriverWait getWait() {
+        return wait;
+    }
+}
